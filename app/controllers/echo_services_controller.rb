c95a390ddf3766a74b74169e1af5f00deebc88fa
Ryan Powell <cpowell@sapient.com>
2016-07-13 12:35:54 -0400

    removing heros/ and echo_services/ that are not used (they were part of boilerplate).

--- app/controllers/echo_services_controller.rb
+++
@@ -1,7 +0,0 @@
-class EchoServicesController < ApplicationController
-  def index
-    respond_to do |format|
-      format.json { render json: params.except(:controller,:action,:format).each { |val| puts val } }
-    end
-  end
-end
--- app/views/heros/show.json.jbuilder
+++
@@ -1,1 +0,0 @@
-json.extract! @hero, :id, :name, :age, :height
