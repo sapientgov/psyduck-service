6636c82ded96c3f5ce62d86e5a7cca050a9b3424
James Caple <capable2@gmail.com>
2016-07-13 14:41:51 -0400

    Simple Office and Staff model tests

--- spec/models/office_spec.rb
+++ spec/models/office_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Office, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+	office = Office.create!
+	office.name = "Oracle"
+	office.address = "1 infinite loop, Cupertino, CA"
+	it "should have a value" do
+		office.name.should eq("Oracle")
+	end
 end
